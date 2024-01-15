3a27a7638491594f81f919e27ac590aebacc4dfd
1337 Cheats <156625452+1337Cheats@users.noreply.github.com>
2024-01-14 21:35:47 -0600

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,21 +1,33 @@
 /*
-1337 Cookie Clicker Utility v0.1
+1337 Cookie Clicker Utility v0.0.2
 author: 1337 Cheats
 description: Cookie Clicker utility mod made by 1337 Cheats!
 */
 
 function autoclicker(cps, clickItem) {
-    var clickInterval = setInterval(function() {var item = document.getElementById(clickItem);item.click();}, cps / 1000);
-  document.addEventListener("keydown", function(event) {
-    if (event.keyCode === 113) {
-        clearInterval(clickInterval);
-    }
-});
+    var clickInterval = setInterval(function() {
+        var item = document.getElementById(clickItem);
+        item.click();
+    }, cps / 1000);
+    document.addEventListener("keydown", function(event) {
+        if (event.keyCode === 113) {
+            clearInterval(clickInterval);
+        }
+    });
 }
 
 function cookieAdder(amnt) {
     Game.Earn(amnt);
 }
+
+function spawnGc(amount) {    
+        for (var i = 0; i < amount; i++) {
+            var newShimmer = new Game.shimmer("golden");
+            console.log(`[+] 1337 Cookie Clicker Util - spawned ${i} golden cookies`);
+        
+    }
+}
+
 
 // Create the wrapper element
 var wrapper = document.createElement("div");
@@ -25,7 +37,7 @@ wrapper.style.left = "0";
 wrapper.style.zIndex = "9999";
 // Create the h3 element
 var heading = document.createElement("h3");
-heading.textContent = "1337 Cookie Clicker Utility Mod v0.0.1";
+heading.textContent = "1337 Cookie Clicker Utility Mod v0.0.2";
 
 // Create the close button element
 var closeButton = document.createElement("button");
@@ -52,12 +64,18 @@ cookieAdderButton.addEventListener("click", function() {
 
 });
 
+var gcSpawnButton = document.createElement("button");
+gcSpawnButton.textContent = "Spawn Golden Cookie(random)";
+gcSpawnButton.addEventListener("click", function() {
+    spawnGc(1);
+});
 
 // Append the h3, close button, autoclicker button, cookieAdder button, and buildingBuy button to the wrapper
 wrapper.appendChild(heading);
 wrapper.appendChild(closeButton);
 wrapper.appendChild(autoclickerButton);
 wrapper.appendChild(cookieAdderButton);
+wrapper.appendChild(gcSpawnButton);
 
 // Append the wrapper to the document body
 document.body.appendChild(wrapper);
